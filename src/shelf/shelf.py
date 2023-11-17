8741c225661b7b1f825dd87a68f5e9c503625ffc
Nicholas Junge <nicholas.junge@web.de>
2023-11-17 07:17:26 +0100

    Add prefixes and registry argument

--- src/shelf/shelf.py
+++ src/shelf/shelf.py
@@ -1,7 +1,17 @@
-from typing import Any
+from typing import Any, Sequence
 
+from .registry import registry
 
 class Shelf:
+
+    def __init__(self, prefixes: Sequence[str]):
+        self.prefixes = tuple(prefixes)
+        self._registry = registry
+
+    @property
+    def registry(self):
+        return self._registry
+
     def get(self, prefix: str, expected_type: type) -> Any:
         pass
 
